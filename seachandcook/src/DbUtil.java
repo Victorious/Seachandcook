b58319201fb2ce155f3a1b8640bc72e6f89deded
Marcus Wilen <marcus.wilen@hotmail.com>
2019-01-27 17:50:33 +0100

    first commit

---
+++ seachandcook/src/DbUtil.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbUtil {
+//	Database connection varaibles
+	
+	private static Statement statm;
+	private static ResultSet rs;
+	private static volatile Connection con = null;
+
+	
+	public static Connection getDbConnection() {
+		 try {	 			
+				 con = DriverManager.getConnection(dbPath + dbName, user, pass);
+				 statm = con.createStatement();
+
+		 } catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return con;
+		
+	}
+	
+	public void getTable() throws SQLException {
+		rs = statm.executeQuery("SELECT * FROM ingredients");
+		
+		while (rs.next()) {
+			System.out.println(rs.getString("ingredient"));
+		}
+	}
+}
