c773d6040217496669ddd433568d4f77729db6d0
Marcus Wilen <marcus.wilen@hotmail.com>
2019-01-30 18:00:05 +0100

    Adding all the files together and creating a test list

--- seachandcook/src/Main.java
+++ seachandcook/src/Main.java
@@ -1,14 +1,20 @@
 import java.awt.EventQueue;
 import java.sql.SQLException;
 
+import com.sun.corba.se.impl.ior.GenericTaggedComponent;
+
 public class Main {
 
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) throws SQLException {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					seachandcookGUI frame = new seachandcookGUI();
 					frame.setVisible(true);
+					DbUtil.getDbConnection();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
